1055ee5b5d34e8f8271e5e511e63774b25554d7f
McLovin <66857486+McLovin-bot@users.noreply.github.com>
2022-09-11 20:51:37 -0400

    init

--- src/main/java/mcscripts/mcmacro/Main.java
+++ src/main/java/mcscripts/mcmacro/Main.java
@@ -6,6 +6,8 @@ import mcscripts.mcmacro.proxy.ClientProxy;
 import mcscripts.mcmacro.proxy.CommonProxy;
 import mcscripts.mcmacro.util.KeyInputHandler;
 import mcscripts.mcmacro.util.Reference;
+import mcscripts.mcmacro.util.httpUtil;
+import mcscripts.mcmacro.util.javaUtils;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
@@ -14,6 +16,7 @@ import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -25,12 +28,19 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import org.lwjgl.input.Keyboard;
 
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.Scanner;
+
 @Mod(
         modid = Reference.MODID,
         name = Reference.NAME,
         version = Reference.VERSION
 )
 public class Main {
+    public static String version = "0.2";
     public static Minecraft mc = Minecraft.getMinecraft();
     public static EntityPlayerSP player = mc.player;
     public static int tick = 0;
@@ -49,15 +59,18 @@ public class Main {
     }
 
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
+    public void init(FMLInitializationEvent event) throws Exception {
+        if (!Objects.equals(httpUtil.httpGet("https://raw.githubusercontent.com/McLovin-bot/McMacro/main/src/main/java/mcscripts/mcmacro/VERSION.txt"), version)) {
+            updateMod
+        }
         proxy.init(event);
         MinecraftForge.EVENT_BUS.register(new KeyInputHandler());
         MinecraftForge.EVENT_BUS.register(new netherwartMain());
         MinecraftForge.EVENT_BUS.register(new Main());
         ClientProxy.keyBindings = new KeyBinding[2];
-        ClientProxy.keyBindings[0] = new KeyBinding("key.macro", Keyboard.KEY_J, "key.categories.McMacro" );
+        ClientProxy.keyBindings[0] = new KeyBinding("key.macro", Keyboard.KEY_J, "key.categories.McMacro");
         //for (int i = 0; i < ClientProxy.keyBindings.length; i++)
-            //ClientRegistry.registerKeyBinding(ClientProxy.keyBindings[i]);
+        //ClientRegistry.registerKeyBinding(ClientProxy.keyBindings[i]);
         ClientRegistry.registerKeyBinding(ClientProxy.keyBindings[0]);
     }
 
